8353d877c083080eaad34d0ff8363614b0c96c18
raul victor <raulvictormello@hotmail.com>
2022-07-16 22:50:59 -0300

    atualizando

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let celulas = document.querySelectorAll('celula');
-let chechar_turno= true;
+let celulas = document.querySelectorAll(".celula");
+let checar_turno= true;
 const jogador_x="x"
 const jogador_o="O"
 
@@ -8,9 +8,56 @@ document.addEventListener('click', (e)=>{
     jogar(e.target.id);
 }
 });
+const combinaçao=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+
+]
+
 function jogar (id){
     const celula = document.getElementById(id);
-    turno = chechar_turno ? jogador_x:jogador_o;
-    celula.innerHTML = turno;
-    chechar_turno = !chechar_turno;
+   turno = checar_turno ? jogador_x : jogador_o;
+   checar_turno=!checar_turno;
+    celula.textContent = turno;
+    celula.classList.add(turno);
+
+    checar_ganhador(turno);
+
 }
+
+function checar_ganhador(turno){
+    const vencedor = combinaçao.some((comb)=>{
+        return comb.every((index)=>{
+          return celulas[index].classList.contains(turno);
+
+        })
+    });
+    if(vencedor){
+        encerrar_jogo(turno);
+    }
+    else if(checar_empate){
+        encerrar_jogo();
+    }
+} //fim da função checar_ganhador
+function checar_empate(){
+    return false
+} 
+
+ function encerrar_jogo(vencedor=null){
+        if(vencedor){
+            console.log(vencedor + " venceu ");
+        }
+           else{
+            console.log("Empate");
+           
+           }
+            
+        }
+        
+    
